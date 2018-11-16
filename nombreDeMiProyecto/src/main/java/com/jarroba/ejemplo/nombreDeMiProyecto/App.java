d48a4ed2126bcb6f25576461851d690ff4022a4f
mildreey <erika@assist.com.co>
2018-11-16 10:26:17 -0500

    mensaje

---
+++ nombreDeMiProyecto/src/main/java/com/jarroba/ejemplo/nombreDeMiProyecto/App.java
@@ -0,0 +1,13 @@
+package com.jarroba.ejemplo.nombreDeMiProyecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
